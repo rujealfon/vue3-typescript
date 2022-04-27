bfcfd1ec1197b2c3c4150a49c4377521db10194e
Ruje Alfon <ruje.alfon@bcstechnology.com.au>
2022-04-27 21:09:53 +0800

    add store type

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,9 +1,15 @@
 import { defineStore } from 'pinia'
 
+export type Item = {
+  name: string;
+  url: string;
+}
+
 export const useCounterStore = defineStore({
   id: 'counter',
   state: () => ({
-    counter: 0
+    counter: 0 as number,
+    items: [] as Item[]
   }),
   getters: {
     doubleCount: (state) => state.counter * 2
