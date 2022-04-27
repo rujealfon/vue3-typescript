a2d7777f7c441f7d6876b3dd8be4e8ae35d8b7dc
Ruje Alfon <ruje.alfon@bcstechnology.com.au>
2022-04-28 07:52:10 +0800

    fix repository

--- src/repository.ts
+++ src/repository.ts
@@ -1,27 +1,29 @@
+import type { AxiosInstance } from 'axios'
+
 export default class Repository {
-  constructor(_axios, _base) {
-    this.axios = _axios
-    this.base = _base
-  }
+  constructor(
+    public axios: AxiosInstance,
+    public base: string
+  ) {}
 
   search(params) {
-    return this.axios.$get(`${this.base}`, { params })
+    return this.axios.get(`${this.base}`, { params })
   }
 
   detail(id) {
-    return this.axios.$get(`${this.base}/${id}`)
+    return this.axios.get(`${this.base}/${id}`)
   }
 
   create(payload) {
-    return this.axios.$post(`${this.base}`, payload)
+    return this.axios.post(`${this.base}`, payload)
   }
 
   update(payload) {
-    return this.axios.$put(`${this.base}/${payload.id}`, payload)
+    return this.axios.put(`${this.base}/${payload.id}`, payload)
   }
 
   delete(id) {
-    return this.axios.$delete(`${this.base}/${id}`)
+    return this.axios.delete(`${this.base}/${id}`)
   }
 
   // restore(id) {
